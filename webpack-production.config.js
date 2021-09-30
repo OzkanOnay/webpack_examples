55b970601d9082bd48dce5ec9da924fdc4fac8ea
Özkan Onay <ozkanonay@gmail.com>
2021-09-30 19:50:34 +0300

    production build

---
+++ webpack-production.config.js
@@ -0,0 +1,13 @@
+var webPackStrip = require('strip-loader');
+var devConfig = require('./webpack.config');
+var stripLoader = {
+    test: [/\.js$/,/\.es6/],
+    exclude:/node_modules/,
+    use : {
+        loader: webPackStrip.loader('console.log')
+    }
+}
+
+devConfig.module.rules.push(stripLoader);
+
+module.exports = devConfig;
